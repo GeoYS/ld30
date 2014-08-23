1e849cc782fc4b3eafe153a608b6a51149bc3a21
Duane Byer <duane.byer@gmail.com>
2014-08-22 23:44:08 -0400

    Fixed the abominable tabs in the main class.

--- src/com/aqua/ludum/ld30/MainGameClass.java
+++ src/com/aqua/ludum/ld30/MainGameClass.java
@@ -5,25 +5,25 @@ import com.badlogic.gdx.ApplicationListener;
 public class MainGameClass implements ApplicationListener {
 	
 	@Override
-	public void create() {		
+	public void create() {
 	}
-
+	
 	@Override
 	public void dispose() {
 	}
-
+	
 	@Override
-	public void render() {		
+	public void render() {
 	}
-
+	
 	@Override
 	public void resize(int width, int height) {
 	}
-
+	
 	@Override
 	public void pause() {
 	}
-
+	
 	@Override
 	public void resume() {
 	}
