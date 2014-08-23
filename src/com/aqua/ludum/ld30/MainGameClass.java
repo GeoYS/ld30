d704fba16dd2e42c3a774eeff91f499d99cfcc3b
George Shen <george.school303@gmail.com>
2014-08-22 23:40:50 -0400

    First push.

---
+++ src/com/aqua/ludum/ld30/MainGameClass.java
@@ -0,0 +1,30 @@
+package com.aqua.ludum.ld30;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class MainGameClass implements ApplicationListener {
+	
+	@Override
+	public void create() {		
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public void render() {		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
