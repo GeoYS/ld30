1cda032cf3f12b7fb39bf5147c04774c5edf4e27
Duane Byer <duane.byer@gmail.com>
2014-08-25 17:20:15 -0400

    2

--- src/com/aqua/ludum/ld30/game/NeutralBuilding.java
+++ src/com/aqua/ludum/ld30/game/NeutralBuilding.java
@@ -18,15 +18,22 @@ public class NeutralBuilding extends SpawnBuilding {
 		return Float.POSITIVE_INFINITY;
 	}
 	
+	@Override
+	public void update(float delta) {
+		this.spiritCount = 1;
+		super.update(delta);
+	}
+	
 	
 	@Override
 	public void spawn() {
 		super.spawn();
+		terrain.spawnUnit(new Worker(terrain.getNeutralPlayer(), new Vector2(0.0f, this.getRadius() + 32.0f).add(position), terrain));
 	}
 
 	@Override
 	public float timeUntilSpawn() {
-		return 10.0f;
+		return 60.0f;
 	}
 
 
